19ad3ed7f7399e9a2b08ec8185e29ee0dd37eed1
Dennis Schwerdel <schwerdel@googlemail.com>
2019-02-14 22:53:08 +0100

    Using cargo deb for packaging + cross compilation

--- build.rs
+++ build.rs
@@ -1,9 +1,25 @@
 // VpnCloud - Peer-to-Peer VPN
-// Copyright (C) 2015-2017  Dennis Schwerdel
+// Copyright (C) 2015-2019  Dennis Schwerdel
 // This software is licensed under GPL-3 or newer (see LICENSE.md)
 
 extern crate cc;
 
+use std::process::Command;
+use std::env;
+use std::path::Path;
+use std::fs;
+
 fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    // Compile tun/tap C code
+    println!("rerun-if-changed=src/c/tuntap.c");
     cc::Build::new().file("src/c/tuntap.c").include("src").compile("libtuntap.a");
+
+    // Process manpage using ronn command
+    println!("rerun-if-changed=vpncloud.md");
+    fs::copy("vpncloud.md", Path::new(&out_dir).join("vpncloud.1.ronn")).unwrap();
+    Command::new("ronn").args(&["-r", "vpncloud.1.ronn"]).current_dir(&Path::new(&out_dir)).status().expect("Failed to process manpage, ronn command missing?");
+    Command::new("gzip").args(&["vpncloud.1"]).current_dir(&Path::new(&out_dir)).status().unwrap();
+    fs::copy(Path::new(&out_dir).join("vpncloud.1.gz"), "target/vpncloud.1.gz").unwrap();
 }
