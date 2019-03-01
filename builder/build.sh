73fd3134a56a1038ebe71e038f388e77a83ce10e
Dennis Schwerdel <schwerdel@googlemail.com>
2019-03-01 16:37:43 +0100

    Build chain for deb packages

---
+++ builder/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function docker_cmd() {
+  DIST=$1
+  CMD=$2
+  docker run -it --rm -v $(pwd)/..:/home/user/code \
+    -v $CACHE/registry:/home/user/.cargo/registry \
+    -v $CACHE/git:/home/user/.cargo/git \
+    -v $CACHE/tmp:/home/user/.cargo/tmp \
+    vpncloud-builder-$DIST bash -c "$CMD"
+}
+
+set -e
+
+cd $(dirname $0)
+
+VERSION=$(grep -e '^version =' ../Cargo.toml | sed -e 's/version = "\(.*\)"/\1/')
+
+mkdir -p cache/{git,tmp,registry}
+CACHE=$(pwd)/cache
+
+mkdir -p ../dist
+
+docker build --rm -f=Dockerfile-deb -t vpncloud-builder-deb .
+
+# x86_64 deb
+docker_cmd deb 'cd code && cargo deb'
+cp ../target/debian/vpncloud_${VERSION}_amd64.deb ../dist/vpncloud_${VERSION}_amd64.deb
+
+# arm7hf deb
+docker_cmd deb 'cd code && cargo deb --target armv7-unknown-linux-gnueabihf'
+cp ../target/armv7-unknown-linux-gnueabihf/debian/vpncloud_${VERSION}_armhf.deb ../dist/vpncloud_${VERSION}_armhf.deb
