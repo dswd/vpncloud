805c4055a07de8959ffc1f6172a699da1100135d
Dennis Schwerdel <schwerdel@googlemail.com>
2020-05-28 09:07:34 +0200

    Added missing copyright headers

--- src/tests/helper.rs
+++ src/tests/helper.rs
@@ -1,3 +1,7 @@
+// VpnCloud - Peer-to-Peer VPN
+// Copyright (C) 2015-2020  Dennis Schwerdel
+// This software is licensed under GPL-3 or newer (see LICENSE.md)
+
 macro_rules! assert_clean {
     ($($node: expr),*) => {
         $(
